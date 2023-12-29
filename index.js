641ad943e1568bbef064dcbdbb5907e3668b4101
LAPTOP-5SG2CNP0\NGUYEN <thainguyen122004@gmail.com>
2023-12-29 20:42:54 +0700

    link render

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
-
+// https://book-management-4qw7.onrender.com
 // middlewear 
 app.use(cors());
 app.use(express.json());
