4688d4a2ff0732a168dc26555801f43023a78249
LAPTOP-5SG2CNP0\NGUYEN <thainguyen122004@gmail.com>
2023-12-29 17:47:06 +0700

    new

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://mern-book-store:1234567890@cluster0.wcl6ubr.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptionsobject to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
